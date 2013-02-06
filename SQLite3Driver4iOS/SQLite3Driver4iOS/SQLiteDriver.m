6b0a3768160bdfd441788324c4d192f1fe03ed2c
MATSUKI.Hidenori <MATSUKI.Hidenori@gmail.com>
2013-02-07 00:30:50 +0900

    Create iOS project.

---
+++ SQLite3Driver4iOS/SQLite3Driver4iOS/SQLiteDriver.m
@@ -0,0 +1,14 @@
+//
+//  SQLiteDriver.m
+//  SQLite3Driver4iOS
+//
+//  Created by Matsuki Hidenori on 2/7/13.
+//  Copyright (c) 2013 Matsuki Hidenori. All rights reserved.
+//
+
+#import "SQLiteDriver.h"
+#import <sqlite3.h>
+
+@implementation SQLiteDriver
+
+@end
---
+++ SQLite3Driver4iOS/SQLite3Driver4iOS/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SQLite3Driver4iOS
+//
+//  Created by Matsuki Hidenori on 2/7/13.
+//  Copyright (c) 2013 Matsuki Hidenori. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
