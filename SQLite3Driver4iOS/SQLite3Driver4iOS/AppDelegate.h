00ff07a6663c5f5de390feb5a97e5880543a2206
MATSUKI.Hidenori <MATSUKI.Hidenori@gmail.com>
2013-02-07 00:30:50 +0900

    Create iOS project.

---
+++ SQLite3Driver4iOS/SQLite3Driver4iOS/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SQLite3Driver4iOS
+//
+//  Created by Matsuki Hidenori on 2/7/13.
+//  Copyright (c) 2013 Matsuki Hidenori. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
