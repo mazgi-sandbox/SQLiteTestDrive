08136157571d03fcfa65b36e9228b6277493de62
MATSUKI.Hidenori <MATSUKI.Hidenori@gmail.com>
2013-02-07 01:11:29 +0900

    Create SQLite driver class for iOS.

---
+++ SQLite3Driver4iOS/SQLite3Driver4iOS/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  SQLite3Driver4iOS
+//
+//  Created by Matsuki Hidenori on 2/7/13.
+//  Copyright (c) 2013 Matsuki Hidenori. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ SQLite3Driver4iOS/SQLite3Driver4iOS/SQLiteDriver.h
@@ -0,0 +1,13 @@
+//
+//  SQLiteDriver.h
+//  SQLite3Driver4iOS
+//
+//  Created by Matsuki Hidenori on 2/7/13.
+//  Copyright (c) 2013 Matsuki Hidenori. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SQLiteDriver : NSObject
+
+@end
