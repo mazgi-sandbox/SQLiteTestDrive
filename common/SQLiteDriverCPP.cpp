424ee3fe8b109b7250f9e35f604ba0777d31baca
MATSUKI.Hidenori <MATSUKI.Hidenori@gmail.com>
2013-02-26 17:52:20 +0900

    Create cpp driver.

---
+++ common/SQLiteDriverCPP.cpp
@@ -0,0 +1,9 @@
+//
+//  SQLiteDriverCPP.cpp
+//  SQLiteTestDrive
+//
+//  Created by MATSUKI Hidenori on 2/26/13.
+//  Copyright (c) 2013 Matsuki Hidenori. All rights reserved.
+//
+
+#include "SQLiteDriverCPP.h"
---
+++ common/SQLiteDriverCPP.h
@@ -0,0 +1,14 @@
+//
+//  SQLiteDriverCPP.h
+//  SQLiteTestDrive
+//
+//  Created by MATSUKI Hidenori on 2/26/13.
+//  Copyright (c) 2013 Matsuki Hidenori. All rights reserved.
+//
+
+#ifndef __SQLite3Driver4iOS__SQLiteDriverCPP__
+#define __SQLite3Driver4iOS__SQLiteDriverCPP__
+
+#include <iostream>
+
+#endif /* defined(__SQLite3Driver4iOS__SQLiteDriverCPP__) */
