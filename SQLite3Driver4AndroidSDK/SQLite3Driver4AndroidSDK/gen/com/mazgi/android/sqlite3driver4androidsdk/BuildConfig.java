1e529e2432bb2d8fe38ab126b6bdff75e2ef9567
MATSUKI.Hidenori <MATSUKI.Hidenori@gmail.com>
2013-02-26 13:23:51 +0900

    Create Android Driver project.

---
+++ SQLite3Driver4AndroidSDK/SQLite3Driver4AndroidSDK/gen/com/mazgi/android/sqlite3driver4androidsdk/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.mazgi.android.sqlite3driver4androidsdk;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
